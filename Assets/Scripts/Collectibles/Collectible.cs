63b99cd21861caaf282f4ec6bb5369120ce43c0d
Eero12 <100197299+Eero12@users.noreply.github.com>
2022-10-06 10:29:33 +0300

    #4?

--- Assets/Scripts/Collectibles/Collectible.cs
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public PlayerHP playerHP;
-    
+    public Animations animations;
+
     public static event Action OnCollected;
     void Update()
     {
@@ -20,13 +21,13 @@ public class Collectible : MonoBehaviour
             OnCollected?.Invoke();
             if (playerHP.playerHP < 100)
             {
-            playerHP.playerHP += 20;
-            Destroy(gameObject);
+                playerHP.playerHP += 20;
+                Destroy(gameObject);
             }
             if (playerHP.playerHP > 100)
             {
                 playerHP.playerHP = 100;
-            }         
+            }
         }
 
         if (other.CompareTag("Player") && this.CompareTag("Armor"))
@@ -45,5 +46,33 @@ public class Collectible : MonoBehaviour
 
 
         }
-    }
-}
+        if (other.CompareTag("Player") && this.CompareTag("Salt_Ammo"))
+        {
+            OnCollected?.Invoke();
+            if (animations.saltgunAmmo < 30)
+            {
+                animations.saltgunAmmo += 5;
+                Destroy(gameObject);
+            }
+            if (animations.saltgunAmmo > 30)
+            {
+                animations.saltgunAmmo = 30;
+            }
+        }
+                if (other.CompareTag("Player") && this.CompareTag("Double_Barrel_Ammo"))
+                {
+                    OnCollected?.Invoke();
+                    if (animations.doubleBarrelAmmo < 30)
+                    {
+                        animations.doubleBarrelAmmo += 5;
+                        Destroy(gameObject);
+                    }
+                    if (animations.doubleBarrelAmmo > 30)
+                    {
+                        animations.doubleBarrelAmmo = 30;
+                    }
+                }
+            }
+        }
+    
+
--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,27 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class EnemyAI : MonoBehaviour
 {
-
     public NavMeshAgent agent;
-
     public Transform player;
-
     public LayerMask Ground, Player;
-
-
     public Vector3 walkPoint;
     bool walkPointset;
     public float walkPointRange;
-
-
-
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
-
     public void Awake()
     {
         player = GameObject.Find("Player").transform;
@@ -29,22 +17,21 @@ public class EnemyAI : MonoBehaviour
     }
     private void Update()
     {
+        //onko pelaaja näkyvissä?
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
-
+        //jos pelaaja ei ole näkyvissä// Patrol()
         if (!playerInSightRange && !playerInAttackRange) Patroling();
+        //onko pelaaja lähellä?
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+        //jos pelaaja on näkyvissä // ChasePlayer()
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        //if (playerInAttackRange && playerInAttackRange) AttackPlayer();
-
     }
-
+    //Pelaajan etsiminen
     private void Patroling()
     {
         if (!walkPointset) SearchWalkPoint();
-
         if (walkPointset)
             agent.SetDestination(walkPoint);
-
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointset = false;
@@ -54,17 +41,15 @@ public class EnemyAI : MonoBehaviour
         Debug.Log("löyty");
         agent.SetDestination(player.position);
     }
-
+    //Satunnainen pelaajan etsiminen
     private void SearchWalkPoint()
     {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
-
+        //Satunnaisen muuttujan(saapumispisteen) luonti = (walkPoint)
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
         if (Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
             walkPointset = true;
-
     }
     private void OnDrawGizmosSelected()
     {
@@ -72,46 +57,7 @@ public class EnemyAI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
-
-
-
     }
-
-    //private void AttackPlayer()
-    //{
-    //    agent.SetDestination(transform.position);
-    //    transform.LookAt(player);    
-
-    //    if (!alreadyAttacked)
-    //    {
-            
-    //     Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-    //        rb.AddForce(transform.forward* 32f, ForceMode.Impulse);
-    //        rb.AddForce(transform.up* 8f, ForceMode.Impulse);
-    //        alreadyAttacked = true;
-    //        Invoke(nameof(ResetAttack), timeBetweenAttacks);
-    //    }
-
-
-    }
-    //private void ResetAttack()
-    //{
-    //alreadyAttacked = false;
-    //}
-
-//public void TakeDamage(int damage)
-//{
-//    hyttyHealth -= damage;
-
-//    if (hyttyHealth <= 0)
-//    {
-//        DestroyEnemy();
-//    }
-//}
-
-//private void DestroyEnemy()
-//{
-//    Destroy(gameObject);
-//}
+   }
 
 
