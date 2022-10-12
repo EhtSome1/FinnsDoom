ec6f6dfd0e0cf1beb6565183194f0992b0646dc3
Alpo Uusitalo <arppauu@gmail.com>
2022-10-12 10:15:28 +0300

    door animation

--- Assets/Scripts/map/canOpenDoor.cs
+++ Assets/Scripts/map/canOpenDoor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class canOpenDoor : MonoBehaviour
 {
+    public Animator dooranim;
+
     public bool hasKey;
 
     void OnTriggerStay(Collider other)
@@ -12,6 +14,7 @@ public class canOpenDoor : MonoBehaviour
         {
             if (hasKey && Input.GetKeyDown(KeyCode.F))
             {
+                dooranim.SetBool("doorOpen", true);
                 Debug.Log("Door opened");
             }
         }
