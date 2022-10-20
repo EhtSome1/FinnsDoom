7bba06ea0d52a17d936b3a4ace57726c4bcef3c3
Porksu <s1mojo00@students.osao.fi>
2022-10-20 11:44:17 +0300

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    private float startTime;
+    public float timer = 0;
+    bool switchedScene = false;
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float t = Time.time;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+        TimingManager.myTimer += Time.deltaTime;
+
+        Debug.Log(TimingManager.myTimer);
+    }
+}
